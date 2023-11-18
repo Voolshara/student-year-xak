0cfd7393eb31715baa55937934b68177ed671331
Stanislav <sta-tun-slav@yandex.ru>
2023-11-18 09:17:55 +0300

    add tree creation

--- frontend/src/components/Thred.tsx
+++ frontend/src/components/Thred.tsx
@@ -66,6 +66,24 @@ export default function ThredElement({ thredData, nowLevel, isLast }: Props) {
           ))
         )}
       </div>
+
+      <svg
+        onClick={() => {
+          push(`/thred/${-1}/${thredData.id}`);
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-8 cursor-pointer"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
     </div>
   );
 }
